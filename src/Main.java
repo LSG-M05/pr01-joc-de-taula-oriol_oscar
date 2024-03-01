b259c8e643680858ce628c0b5ba42be4f0756b95
Oriol <oriolfontcu@gmail.com>
2024-03-01 10:18:31 +0100

    Estructura Modificada

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 import java.util.Objects;
+import java.util.Random;
 
 public class Main {
 
-    public static String[] diccionari = {"id", "rol", "esta_viu", "enamorat", "esta_protegit"};
+    static Random random = new Random();
+
+    public static String[] diccionari = {"id", "rol", "esta_viu", "enamorat"};
 
     /*
 
        Llegenda:
 
             ID's -->
-                Tenim 8 id's desde 0 fins al 7, que son els jugadors que hi ha.
+                Tenim 5 id's desde 0 fins al 4, que son els jugadors que hi ha.
 
             ROL -->
                 1 --> Aledeano (2)
-                2 --> Lobo (2)
+                2 --> Lobo (1)
                 3 --> Cupído (1)
-                4 --> Vidente (1)
-                5 --> Protector (1)
-                6 --> Cazador (1)
+                4 --> Cazador (1)
 
             ESTA_VIU -->
                 El número 1 vol dir que el jugador està viu, i el 0 vol dir que està mort.
@@ -25,21 +26,17 @@ public class Main {
             ENAMORAT -->
                 El número 1 vol dir que el jugador està enamorat, i el 0 vol dir que no hi esta.
 
-            PROTEGIT -->
-                El número 1 vol dir que el jugador està protegit, i el 0 vol dir que no hi esta.
-
      */
     public static int[][] jugadors = {
-            { 0 , 1 , 1 , 0 , 0 },
-            { 1 , 1 , 1 , 0 , 0 },
-            { 2 , 2 , 1 , 0 , 0 },
-            { 3 , 2 , 1 , 0 , 0 },
-            { 4 , 3 , 1 , 0 , 0 },
-            { 5 , 4 , 1 , 0 , 0 },
-            { 6 , 5 , 1 , 0 , 0 },
-            { 7 , 6 , 1 , 0 , 0 },
+            { 0 , 1 , 1 , 0 },
+            { 1 , 1 , 1 , 0 },
+            { 2 , 2 , 1 , 0 },
+            { 3 , 3 , 1 , 0 },
+            { 4 , 4 , 1 , 0 },
     };
     public static int[] jugadorExpulsat = new int[1];
+
+    public static int idUsuari = random.nextInt(jugadors.length);
     public static int buscarId(String buscar){
         boolean trobat = false;
         int i = -1;
